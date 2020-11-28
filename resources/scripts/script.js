fff1d0cff9befa6e0ef7e14fbc3d31cc3c974c0b
Samy Adel <samy.adel@gembitz.com>
2020-11-28 12:54:49 +0000

    Added gradient, site header and updated filter choices to match the actual image

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,9 +1,17 @@
 //***************DOM SELECTORS***************/
 
 const filters = [...document.querySelectorAll(".filters *")];
+const filterImages = [...document.querySelectorAll(".filter-choice img")];
 const imageUploader = document.getElementById("image-uploader");
+const hidePresets = document.getElementById("hide-presets");
 const image = document.getElementById("filter");
 const rangeInputs = [...document.querySelectorAll("input[type='range']")];
+const overlayOptions = document.querySelectorAll("input[name='overlay']");
+const solidBackgroundOption = document.getElementById("solid");
+const colorPicker = document.getElementById("color");
+const opacity = document.getElementById("opacity");
+
+document.querySelector(".solid-overlay-options").hidden = true;
 
 const imageFilters = {
   contrast: "100",
@@ -15,6 +23,8 @@ const imageFilters = {
   "hue-rotate": "0",
   blur: "0",
 };
+
+filterImages.forEach((el) => (el.src = image.children[0].src));
 
 filters.forEach((filter) => {
   filter.addEventListener("click", (e) => {
@@ -40,12 +50,30 @@ imageUploader.addEventListener("change", () => {
 
   reader.onloadend = function () {
     image.children[0].src = reader.result;
+    filterImages.forEach((el) => (el.src = image.children[0].src));
   };
 
   if (file) {
     reader.readAsDataURL(file);
   } else {
-    image.children[0].src.src = "";
+    image.children[0].src = "";
+  }
+});
+
+hidePresets.addEventListener("click", () => {
+  document.querySelector(".main-viewport").classList.toggle("fullscreen");
+  if (
+    [...document.querySelector(".main-viewport").classList].indexOf(
+      "fullscreen"
+    ) !== -1
+  ) {
+    document.querySelector(
+      "label[for='hide-presets']"
+    ).innerHTML = `<i class="fas fa-sliders-h"></i> Show Presets`;
+  } else {
+    document.querySelector(
+      "label[for='hide-presets']"
+    ).innerHTML = `<i class="fas fa-sliders-h"></i> Hide Presets`;
   }
 });
 
@@ -68,5 +96,18 @@ rangeInputs.forEach((el) => {
     }
 
     image.style.filter = filters;
+  });
+});
+
+overlayOptions.forEach((el) => {
+  el.addEventListener("change", (e) => {
+    if (e.target.id === "solid") {
+      document.querySelector(".solid-overlay-options").hidden = false;
+
+      image.classList.add("custom-filter");
+      // console.log(document.querySelector(".custom-filter::before"));
+    } else {
+      document.querySelector(".solid-overlay-options").hidden = true;
+    }
   });
 });
